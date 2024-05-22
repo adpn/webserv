afd4ab7298c08099143efb575e26468c9f9cb211
bvercaem <bvercaem@student.s19.be>
2024-05-22 17:57:25 +0200

    http request parsing works. still needs more checks (especially for headers)

--- includes/Request.hpp
+++ includes/Request.hpp
@@ -8,13 +8,20 @@ using std::string;
 
 class Request
 {
-	static string valid_headers[];
-	static int	  valid_headers_size;
-
+	bool valid;
 	char method;		// 'G' or 'P' or 'D'
 	string uri;
+	string version;
 	string body;
-	std::map<string, string> headers; // maybe sumn else i dunno
+	std::map<string, string> headers; // merge duplicate headers like so [value1], [value2]
+										// exception for 'Set-Cookie' and 'Cookie' headers -> [value1]; [value2]
+
+	// handle errors of these. with (mandatory default) error responses
+	bool parseMethod(string const& method);
+	bool parseUri(string const& uri);
+	bool parseVersion(string const& version);
+	bool parseBody(string const& body);
+	bool parseHeader(string const& header);
 
 public:
 	Request();
@@ -24,10 +31,13 @@ public:
 
 	char getMethod() const;
 	string const& getUri() const;
+	string const& getVersion() const;
 	string const& getBody() const;
 	std::map<string, string> const& getHeaders() const;
 
 	bool parse(string const& package);
+
+	void print(bool do_body = true) const;
 };
 
 #endif
@@ -35,12 +45,12 @@ public:
 /*
 		~~HTTP request~~			SP = space , CRLF = carriage return / line feed
 
-Request-Line ;				->	'Method' SP 'Request-URI' SP 'HTTP-Version' CRLF
+Request-Line CRLF				->	'Method' SP 'Request-URI' SP 'HTTP-Version' CRLF
 *(( general-header ;
 request-header ;
-entity-header ) CRLF) ;
-/n
-CRLF [ message-body ] ;
+entity-header ) CRLF)
+CRLF
+[ message-body ] CRLF (i think it has to end with a CRLF ?)
  */
 
  // methods: GET, POST and DELETE
--- srcs/Request.cpp
+++ srcs/Request.cpp
@@ -1,12 +1,9 @@
 #include "Request.hpp"
-
-/* STATICS */
-valid_headers = {"GET", "POST", "DELETE"};
-valid_headers_size = 3;
+#include <sstream>
 
 /* CONSTRUCTORS */
 Request::Request()
-	: method(0) {}
+	: valid(false), method(0) {}
 
 Request::Request(Request const& src)
 	{ this->operator=(src); }
@@ -16,8 +13,10 @@ Request::~Request()
 
 Request& Request::operator=(Request const& rhs)
 {
+	valid = rhs.valid;
 	method = rhs.method;
 	uri = rhs.uri;
+	version = rhs.version;
 	body = rhs.body;
 	headers = rhs.headers;
 	return *this;
@@ -27,30 +26,132 @@ Request& Request::operator=(Request const& rhs)
 char Request::getMethod() const
 	{ return method; }
 
-string Request::getUri() const
+string const& Request::getUri() const
 	{ return uri; }
 
-string Request::getBody() const
+string const& Request::getVersion() const
+	{ return version; }
+
+string const& Request::getBody() const
 	{ return body; }
 
-std::map<string, string> Request::getHeaders() const
+std::map<string, string> const& Request::getHeaders() const
 	{ return headers; }
 
 /* MEMBERS */
 bool Request::parse(string const& package)
 {
-	// parse and fill class
-	// e.g.
-	int i;
-	for (i = 0; i < valid_headers_size; ++i)
-		if (!package.compare(0, valid_headers[i].size(), valid_headers[i]))
-			break ;
-	if (i == valid_headers_size || package[valid_headers[i].size()] != ' ')
+	std::istringstream iss(package);
+	string token;
+	std::getline(iss, token, ' ');
+	if (!parseMethod(token))
 		return false;
-	// send back an error response here ! (blabla unsupported method blabla)
-	// maybe have a class specific error value/string ?
-			// -> but we do need to implement default error responses
-	method = package[0];
+	std::getline(iss, token, ' ');
+	if (!parseUri(token))
+		return false;
+	std::getline(iss, token);
+	if (!parseVersion(token))
+		return false;
+	if (iss.eof())
+	{
+		valid = true;
+		return true;
+	}
+	std::getline(iss, token);
+	while (!token.empty())
+	{
+		if (!parseHeader(token))
+			return false;
+		if (iss.eof())
+		{
+			valid = true;
+			return true;
+		}
+		std::getline(iss, token);
+	}
+	std::ostringstream oss;
+	oss << iss.rdbuf();
+	if (!parseBody(oss.str()))
+		return false;
+	valid = true;
+	return true;
+}
 
-	// ...
+bool Request::parseMethod(string const& method)
+{
+	string	valid_headers[] = {"GET", "POST", "DELETE"};
+	int		size = 3;
+
+	if (method.size() < 3)
+		return false;
+	for (--size; size >= 0; --size)
+		if (!method.compare(valid_headers[size]))
+			break ;
+	if (size < 0)
+		return false;
+	this->method = method[0];
+	return true;
+}
+
+// basic parsing, don't allow general "*" (n/a for GET, POST or DELETE)
+bool Request::parseUri(string const& uri)
+{
+	if (uri.size() < 2)
+		return false;
+	if (uri.compare(0, 7, "http://") && uri[0] != '/')
+		return false;
+	this->uri = uri;
+	return true;
+}
+
+// only allows HTTP/1.1 (for now)
+bool Request::parseVersion(string const& version)
+{
+	if (version.compare("HTTP/1.1"))
+		return false;
+	this->version = version;
+	return true;
+}
+
+bool Request::parseHeader(string const& header)
+{
+	std::pair<string, string> pair;
+	size_t colon_pos = header.find(':');
+	if (colon_pos == string::npos)
+		return false;
+	pair.first = header.substr(0, colon_pos);
+	while (header[colon_pos + 1] == ' ')
+		++colon_pos;
+	pair.second = header.substr(colon_pos + 1, string::npos);
+
+	// do some parsing and comparing to allowed headers here ...
+
+	std::pair<std::map<string, string>::iterator, bool> ret(this->headers.insert(pair));
+	if (!ret.second)
+		if (ret.first->second.find(pair.second) == string::npos)
+			ret.first->second.append("," + pair.second);
+	return true;
+}
+
+// doesn't check anything
+bool Request::parseBody(string const& body)
+{
+	this->body = body;
+	return true;
+}
+
+/* DEBUG */
+void Request::print(bool do_body) const
+{
+	std::cout << "request package:";
+	if (!valid)
+		std::cout << " INVALID";
+	std::cout << "\n";
+	if (!method)
+		return ;
+	std::cout << "\t" << method << " " << uri << " " << version << "\n";
+	for (std::map<string, string>::const_iterator it = headers.begin(); it != headers.end(); ++it)
+		std::cout << "\t" << (*it).first << ":" << (*it).second << "\n";
+	if (do_body && body.size())
+		std::cout << "\n\t" << body << "\n";
 }
