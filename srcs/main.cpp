fde63d46c2d0227a7135f17d58bd582a2bbc70fa
adupin <adupin@student.s19.be>
2024-05-28 16:25:15 +0200

    Pollout test

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -59,16 +59,55 @@ int managePollin(std::vector<pollfd>& fds, std::vector<Socket>& serverSockets, s
 
 int managePollout(std::vector<pollfd>& fds, size_t i) {
 	std::cout << "Ready to send data to client on fd " << fds[i].fd << std::endl;
-    // Send data to client
-    const char *msg = "Hello from server";
-    ssize_t bytesSent = send(fds[i].fd, msg, strlen(msg), 0);
-    if (bytesSent < 0) {
-        std::cout << "Send error" << std::endl;
-        return 1; //should I stop the program if send fails ????
-    }
-    // After sending data, disable POLLOUT until more data needs to be sent
-    fds[i].events = POLLIN;
-    return 0;
+	// Send data to client
+	// const char *msg = "Hello from server";
+	const char *htmlPage = 
+		"<!DOCTYPE html>"
+		"<html lang=\"en\">"
+		"<head>"
+		"<meta charset=\"UTF-8\">"
+		"<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">"
+		"<title>Hello Bert</title>"
+		"<!-- <style>"
+		"</style> -->"
+		"</head>"
+		"<body>"
+		"<header>"
+		"<h1>Hello Bert</h1>"
+		"</header>"
+		"<nav>"
+		"<!-- Navigation menu can go here -->"
+		"</nav>"
+		"<main>"
+		"<section>"
+		"<!-- <h2>Section 1</h2> -->"
+		"<p>You are not commiting.</p>"
+		"</section>"
+		"<section>"
+		"<!-- <h2>Section 2</h2> -->"
+		"<p>You should commit more.</p>"
+		"</section>"
+		"</main>"
+		"<footer>"
+		"<p>&copy; I don't care</p>"
+		"</footer>"
+		"</body>"
+		"</html>";
+
+	std::string httpResponse =
+		"HTTP/1.1 200 OK\r\n"
+		"Content-Type: text/html\r\n"
+		"Content-Length: " + std::to_string(strlen(htmlPage)) + "\r\n"
+		"\r\n" + htmlPage;
+
+	ssize_t bytesSent = send(fds[i].fd, httpResponse.c_str(), httpResponse.size(), 0);
+	if (bytesSent < 0) {
+		std::cout << "Send error" << std::endl;
+		return 1; //should I stop the program if send fails ????
+	}
+	// After sending data, disable POLLOUT until more data needs to be sent
+	fds[i].events = POLLIN;
+	return 0;
 }
 
 int main()
