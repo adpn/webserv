a15f0165e7304e670f88e8390761f8862f4ee1c2
adupin <adupin@student.s19.be>
2024-06-04 17:12:17 +0200

    Update managePollout in main

--- srcs/Response.cpp
+++ srcs/Response.cpp
@@ -127,7 +127,6 @@ string Response::wrap_package() const
 	std::ostringstream package;
 
 	package << version << " " << status << " " << reason << "\r\n";
-	package << version << " " << status << " " << reason << "\r\n";
 	for (std::map<string, string>::const_iterator it = headers.begin(); it != headers.end(); ++it)
 		package << (*it).first << ":" << (*it).second << "\r\n";
 	package << "\r\n";
